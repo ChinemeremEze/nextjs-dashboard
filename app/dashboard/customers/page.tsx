dcc89c8bcdbbc50d4368c1e874346fc8b8916c4f
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2024-07-14 19:17:13 -0400

    next js tutorial

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { lusitana } from '../../ui/fonts'
+export default function Page() {
+    return (
+        <div className={`${lusitana.className}`}>Customer Page</div>
+    )
+}
